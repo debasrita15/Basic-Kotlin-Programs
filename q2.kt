58ef810ccf4f20c733eada59b8daa966eed8d516
Debasrita Das <cst.23bcta65@silicon.ac.in>
2026-05-28 14:30:01 +0530

    Added q2

---
+++ q2.kt
@@ -0,0 +1,9 @@
+// Write a program to check whether a given number is even or odd. 
+fun main() {
+    val num = 17
+    if (num % 2 == 0) {
+        println("$num is Even")
+    } else {
+        println("$num is Odd")
+    }
+}
